5a336aed6223fae93702ce21cc0b75c42c2251bd
kj-im <kjim@suprema.co.kr>
2025-02-14 10:39:44 +0900

    update componentes

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -3,18 +3,26 @@ import Include from '@site/src/components/Include';
 import Xclude from '@site/src/components/Xclude';
 import { Steps, Step } from '@site/src/components/Steps';
 import { Folder, File } from '@site/src/components/Files';
+import Head from '@site/src/components/Head';
+import DocsSidebar from '@site/src/components/Toc';
+import Collection from "@site/src/components/Collection";
 import Kbd from '@site/src/components/Kbd';
 import Tabs from '@theme/Tabs';
 import TabItem from '@theme/TabItem';
+import DocCardList from '@theme/DocCardList';
 export default {
     ...MDXComponents,
     Include,
     Xclude,
+    Steps,
+    Step,
     Folder,
     File,
+    Head,
+    DocsSidebar,
+    Collection,
     Kbd,
     Tabs,
     TabItem,
-    Steps,
-    Step
+    DocCardList
 }
