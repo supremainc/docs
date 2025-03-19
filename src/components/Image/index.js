95c4bd46a97b190bbb7f1b3cda07f476e7d793db
kj-im <kjim@suprema.co.kr>
2025-03-20 08:32:12 +0900

    이미지 컴포넌트에 캡션 추가 및 다국어 지원 개선

--- src/components/Image/index.js
+++ src/components/Image/index.js
@@ -2,8 +2,9 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import MDXContents from '@theme-original/MDXContent';
 import clsx from 'clsx';
+import { translate } from '@docusaurus/Translate';
 
-export default function Image({src, alt, className, alone}) {
+export default function Image({src, alt, className, alone, caption}) {
     const { i18n: { currentLocale } } = useDocusaurusContext();
     const imagePath = 
         currentLocale === 'ko' || alone ? 
@@ -19,15 +20,32 @@ export default function Image({src, alt, className, alone}) {
 
     return (
         <MDXContents>
-            <p className='hasimg'>
-                <img
-                    loading="lazy"
-                    src={imagePath}
-                    alt={alt}
-                    className={className}
-                    onError={onError}
-                />
-            </p>
+            {caption ? 
+                <figure>
+                    <img
+                        loading="lazy"
+                        src={imagePath}
+                        alt={alt}
+                        className={clsx('img', className)}
+                        onError={onError}
+                    />
+                    <figcaption>
+                        {translate({
+                            id: 'theme.figureCaption.desc',
+                            message: '위 이미지는 예시 화면입니다. 실제 화면과 다를 수 있습니다.'
+                        })}
+                    </figcaption>
+                </figure>
+                :<p className='hasimg'>
+                    <img
+                        loading="lazy"
+                        src={imagePath}
+                        alt={alt}
+                        className={className}
+                        onError={onError}
+                    />
+                </p>
+                }
         </MDXContents>
     );
 }
