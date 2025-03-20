fa0eace80ccb76f6d587848549f821f4c47b08f2
kj-im <kjim@suprema.co.kr>
2025-03-20 11:27:14 +0900

    이미지 컴포넌트에 아이콘 지원 추가 및 스타일 개선

--- src/components/Image/index.js
+++ src/components/Image/index.js
@@ -4,7 +4,7 @@ import MDXContents from '@theme-original/MDXContent';
 import clsx from 'clsx';
 import { translate } from '@docusaurus/Translate';
 
-export default function Image({src, alt, className, alone, caption}) {
+export default function Image({src, alt, className, alone, caption, ico}) {
     const { i18n: { currentLocale } } = useDocusaurusContext();
     const imagePath = 
         currentLocale === 'ko' || alone ? 
@@ -18,34 +18,47 @@ export default function Image({src, alt, className, alone, caption}) {
         e.target.src = errTarget;
     }
 
-    return (
-        <MDXContents>
-            {caption ? 
-                <figure>
-                    <img
-                        loading="lazy"
-                        src={imagePath}
-                        alt={alt}
-                        className={clsx('img', className)}
-                        onError={onError}
-                    />
-                    <figcaption>
-                        {translate({
-                            id: 'theme.figureCaption.desc',
-                            message: '위 이미지는 예시 화면입니다. 실제 화면과 다를 수 있습니다.'
-                        })}
-                    </figcaption>
-                </figure>
-                :<p className='hasimg'>
-                    <img
-                        loading="lazy"
-                        src={imagePath}
-                        alt={alt}
-                        className={className}
-                        onError={onError}
-                    />
-                </p>
-                }
-        </MDXContents>
-    );
+    if (ico) {
+        return (
+            <img
+                loading="lazy"
+                src={imagePath}
+                alt={alt}
+                className={clsx('ico', className)}
+                onError={onError}
+            />
+        );
+    } else {
+        return (
+            <MDXContents>
+                {caption ? 
+                    <figure>
+                        <img
+                            loading="lazy"
+                            src={imagePath}
+                            alt={alt}
+                            className={clsx('img', className)}
+                            onError={onError}
+                        />
+                        <figcaption>
+                            {translate({
+                                id: 'theme.figureCaption.desc',
+                                message: '위 이미지는 예시 화면입니다. 실제 화면과 다를 수 있습니다.'
+                            })}
+                        </figcaption>
+                    </figure>
+                    :<p className='hasimg'>
+                        <img
+                            loading="lazy"
+                            src={imagePath}
+                            alt={alt}
+                            className={className}
+                            onError={onError}
+                        />
+                    </p>
+                    }
+            </MDXContents>
+        );
+    }
+
 }
