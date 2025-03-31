3997b3b9469cd63ef86c5dcffa1432b4742b1b9e
kj-im <kjim@suprema.co.kr>
2025-03-31 09:52:12 +0900

    다국어 지원 개선: 새로운 로케일 추가 및 중첩 JSON 값 접근 기능 구현

--- src/components/Cmd/index.js
+++ src/components/Cmd/index.js
@@ -4,6 +4,8 @@ import { translate } from '@docusaurus/Translate';
 import clsx from 'clsx';
 import koLocale from './ko.json';
 import enLocale from './en.json';
+import xkoLocale from './x/ko.json';
+import xenLocale from './x/en.json';
 
 // 다국어 지원을 위한 locale mapping
 const localeMap = {
@@ -14,14 +16,34 @@ const localeMap = {
   // es: esLocale,
 };
 
-const ReplacementLocaleText = ({ sid, code, className, children }) => {
+const xlocaleMap = {
+  ko: xkoLocale,
+  en: xenLocale,
+  // 향후 다른 언어 추가 예시:
+  // fr: frLocale,
+  // es: esLocale,
+};
+
+// 중첩된 JSON 값에 접근하기 위한 함수
+const getLocaleText = (locale, sid) => {
+  return sid.split('.').reduce((acc, key) => acc && acc[key], locale);
+};
+
+const ReplacementLocaleText = ({ sid, code, className, children, product }) => {
   const { i18n: { currentLocale } } = useDocusaurusContext();
 
   if (sid) {
     // 현재 로케일에 해당하는 locale을 사용, 없으면 기본은 영어
     const locale = localeMap[currentLocale] || localeMap.en;
-    const localeText = locale[sid] ? locale[sid].replace('<br>', '') : null;
-
+    let localeText;
+    if (product === 'x') {
+      const text = getLocaleText(locale, sid);
+      localeText = text ? text.replace('<br>', '') : null;
+    } else {
+      localeText = locale[sid] ? locale[sid].replace('<br>', '') : null;
+    }
+    
+    
     if (!localeText) {
       console.error(`Locale text not found for SID: ${sid}`);
       return null;
