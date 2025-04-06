05b6d5a288776734930458342942f1fbc82ac320
kj-im <kjim@suprema.co.kr>
2025-04-07 07:56:39 +0900

    문서 수정: 영상 제어하기 섹션 추가 및 재생/일시 정지 아이콘 추가

--- src/components/Columns/index.js
+++ src/components/Columns/index.js
@@ -2,7 +2,7 @@ import clsx from 'clsx';
 
 export default function Columns({ children , className , style }) {
   return (
-    <div className="container center">
+    <div className="container center columns">
         <div className={clsx('row' , className)} style={style} > 
             {children}    
         </div>
