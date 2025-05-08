8b360e45139f8408d9b1342f5bfbd77849368ee6
kj-im <kjim@suprema.co.kr>
2025-05-09 07:50:56 +0900

    feat: biostation3 사양 update

---
+++ src/components/Specs/index.js
@@ -0,0 +1,9 @@
+import Face from '@site/static/img/menus/spec-credential-face.svg';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import {translate} from '@docusaurus/Translate';
+import styles from './styles.module.css';
+import bs3specs from '@site/static/specs/bs3specs.json';
+
+const { i18n: { currentLocale } } = useDocusaurusContext();
+
