d558102068e6ee4d6bdae04ec188a6e748b1fe8c
kj-im <kjim@suprema.co.kr>
2025-05-08 14:02:48 +0900

    feat: Productpart 컴포넌트 추가

---
+++ src/components/Productpart/index.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+import clsx from 'clsx';
+import styles from './styles.module.css';
+
+export function Productpart({ src, maxHeight, maxWidth, children }) {
+  return (
+    <div className='overflow-x'>
+      <div className={styles.product_parts}>
+        <img src={useBaseUrl(src)} className='none' style={{maxHeight: maxHeight, maxWidth: maxWidth}} />
+        { children }
+      </div>
+    </div>
+  )
+}
+
+export function Callout({ children, width, position}) {
+  return (
+    <>
+      {React.Children.map(children, child =>
+        React.isValidElement(child)
+          ? React.cloneElement(child, { width, position })
+          : child
+      )}
+    </>
+  )
+}
+
+export function Item({children, width, position, top}) {
+  let aligndir;
+  if (position === 'left') aligndir = 'right';
+  if (position === 'right') aligndir = 'left';
+
+  return (
+    <div 
+      className={clsx(styles.absolute, styles.desc)}
+      style={{
+        width: width, 
+        top: top, 
+        ...(position === 'left' ? {left: 0} : {right: 0}), 
+        textAlign: aligndir
+      }}
+    >
+      {children}
+    </div>
+
+  )
+}
--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -25,6 +25,7 @@ import Separator from '@site/src/components/Separator';
 import Badge from '@site/src/components/Badge';
 import BugLists from '@site/src/components/BugLists';
 import Jira from '@site/src/components/Jira';
+import { Productpart, Callout, Item } from '@site/src/components/Productpart';
 // icon images svg
 import IcLicense from '@site/static/img/menus/ico-license.svg';
 import IcAdd from '@site/static/img/menus/ico-add.svg';
@@ -90,6 +91,7 @@ export default {
     Badge,
     BugLists,
     Jira,
+    Productpart, Callout, Item,
     IcLicense,
     IcAdd,
     IcClose,
