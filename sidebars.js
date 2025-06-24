71a0b70f1fbfd0ad6f0c699aaf5b040a4db96650
minacong <mashin@suprema.co.kr>
2025-06-24 13:02:01 +0900

    카드 프린터 국문 추가

--- sidebars.js
+++ sidebars.js
@@ -290,6 +290,8 @@ const sidebars = {
               ]
             },
             'platform/biostar_x/settings-diretory-integration',
+            'platform/biostar_x/settings-card-printer',
+            'platform/biostar_x/settings-email-setting',
             'platform/biostar_x/settings-custom-interface',
             {
               type: 'category',
