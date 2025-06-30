41f24695dd2e6bdba03b43ff642e4aa2f088e2fb
kj-im <kjim@suprema.co.kr>
2025-06-30 15:10:06 +0900

    홈 페이지 리디렉션 경로를 useBaseUrl로 수정

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import { Redirect } from '@docusaurus/router';
+import useBaseUrl from '@docusaurus/useBaseUrl';
 
 export default function Home() {
-  return <Redirect to="/help/platform/biostar_x" />;
+  return <Redirect to={useBaseUrl("/platform/biostar_x")} />;
 }
