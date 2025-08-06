a6be93f62a44c5de05cadd840934d53e13ed0af1
kj-im <kjim@suprema.co.kr>
2025-08-06 15:33:03 +0900

    update json2dot.js

--- json2dot.js
+++ json2dot.js
@@ -3,8 +3,8 @@ const path = require('path');
 const {convert} = require('convert-json-to-dot/convert');
 
 // const object = path.join(__dirname, 'src/components/Cmd/x/x.json');
-const object = path.join('C:\\Program Files\\BioStar X\\nginx\\html\\locales\\en\\', 'bs2.json');
-const output = path.join(__dirname, 'src/components/Cmd/en.json');
+const object = path.join('C:\\Program Files\\BioStar X\\nginx\\html\\locales\\en\\', 'messages.json');
+const output = path.join(__dirname, 'src/components/Cmd/x/en.json');
 
 fs.readFile(object, 'utf-8', (err, data) => {
     if (!err) {
