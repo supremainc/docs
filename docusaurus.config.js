a0924934c1cd6d59a210b9a35e1e72e71a1c5a78
kj-im <kjim@suprema.co.kr>
2025-09-25 08:25:27 +0900

    VMS 인증서 설치 안내 추가

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -233,7 +233,7 @@ const config = {
         copyright: getLocalizedConfigValue('copyright'),
       },
       prism: {
-        additionalLanguages: [ 'ini', 'sql', 'excel-formula', 'python' ]
+        additionalLanguages: [ 'ini', 'sql', 'excel-formula', 'python', 'java' ]
       },
       zoom: {
         selector: '.markdown :not(em, div) > img:not(.ico)',
