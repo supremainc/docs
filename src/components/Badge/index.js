0710ac7e8781944b06d38fc8c575a66b076a3e6e
kj-im <kjim@suprema.co.kr>
2025-10-13 11:20:48 +0900

    Badge 컴포넌트에 Docusaurus 번역 기능 추가

--- src/components/Badge/index.js
+++ src/components/Badge/index.js
@@ -1,4 +1,5 @@
 import clsx from 'clsx';
+import { translate } from '@docusaurus/Translate';
 import PropTypes from 'prop-types';
 
 export default function Badge({ children, className, only = false }) {
