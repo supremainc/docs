d0ddc3341e48ac84df230c3ae76b113de46241fd
kj-im <kjim@suprema.co.kr>
2025-10-13 09:44:57 +0900

    Magnify 컴포넌트의 이미지 경로 생성 로직 개선 및 환경에 따른 URL 처리 추가

--- src/components/Magnify/index.js
+++ src/components/Magnify/index.js
@@ -2,7 +2,15 @@ import Magnifier from "react-magnifier";
 import useBaseUrl from '@docusaurus/useBaseUrl';
 
 export default function Magnify(props) {
-    const img = useBaseUrl(props.src);
+    const isDev = process.env.NODE_ENV === 'development';
+    const baseUrl = 'https://supremainc.github.io/docs';
+    const img = (() => {
+        if (isDev) {
+            return useBaseUrl(props.src);
+        } else {
+            return `${baseUrl}${props.src}`;
+        }
+    })();
     const updatedProps = {
         ...props,
         src: img
