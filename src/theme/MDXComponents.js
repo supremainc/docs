2c562c3ecb30a877a9f35f92c12b319b298571cf
kj-im <kjim@suprema.co.kr>
2025-11-18 14:25:01 +0900

    AB#48 Air 새 사용자 문서 추가 작업 완료

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -127,6 +127,7 @@ import IcTnaExpand from '@site/static/img/menus/ico-tna-expand.svg';
 import IcInfinity from '@site/static/img/menus/ico-infinity.svg';
 import IcAirEdit from '@site/static/img/menus/ico-air-edit.svg';
 import IcAirRemove from '@site/static/img/menus/ico-air-remove.svg';
+import IcAirMore from '@site/static/img/menus/ico-air-more.svg';
 export default {
     ...MDXComponents,
     Cmd,
@@ -199,5 +200,5 @@ export default {
     IcBack,
     IcAdvSearch, IcSearch,
     IcFw, IcBw, IcFirst, IcLast, IcAscend, IcDescend, IcSet, IcEdit, IcEdit2, IcEditUL,
-    IcFw2, IcBw2, IcFirst2, IcLast2, IcSet2, IcTslot, IcEraser, IcCalc, IcCopy2, IcRefresh, IcInfo, IcLogout, IcTips, IcLrot, IcRrot, IcCrop, IcFremove, IcCamrot, IcDorder, IcCurP, IcCamFail, IcCamOk, IcMapmore, IcAlert, IcVidLog, IcReportVid, IcViewAlert, IcRepTrash, IcRepoEdit, IcZoom, IcFLockDr, IcFUlockDr, IcRCplay, IcRCstop, IcRCdone, IcRCinfo, IcRCmemo, IcImgLog, IcList, IcTnaExpand, IcInfinity, IcAirEdit, IcAirRemove
+    IcFw2, IcBw2, IcFirst2, IcLast2, IcSet2, IcTslot, IcEraser, IcCalc, IcCopy2, IcRefresh, IcInfo, IcLogout, IcTips, IcLrot, IcRrot, IcCrop, IcFremove, IcCamrot, IcDorder, IcCurP, IcCamFail, IcCamOk, IcMapmore, IcAlert, IcVidLog, IcReportVid, IcViewAlert, IcRepTrash, IcRepoEdit, IcZoom, IcFLockDr, IcFUlockDr, IcRCplay, IcRCstop, IcRCdone, IcRCinfo, IcRCmemo, IcImgLog, IcList, IcTnaExpand, IcInfinity, IcAirEdit, IcAirRemove, IcAirMore
 }
