811a7162a8308a644c88ad474cf754f9a36ae67a
minacong <mashin@suprema.co.kr>
2025-11-24 17:00:27 +0900

    XPass Q2 관련 문서 카테고리 추가

--- sidebars.js
+++ sidebars.js
@@ -1718,6 +1718,42 @@ const sidebars = {
       ],
     },
   ],
+  xpassq2: [
+    {
+      type: 'category',
+      label: 'XPass Q2',
+      collapsed: false,
+      collapsible: false,
+      className: 'p-title',
+      link: {
+        type: 'doc',
+        id: 'device/xpass_q2/index',
+      },
+      items: [
+        'device/xpass_q2/safety-instructions',
+        'device/xpass_q2/getting-started',
+        'device/xpass_q2/installation',
+        'device/xpass_q2/product-specifications',
+        'device/xpass_q2/regulatory-information',
+        'device/xpass_q2/appendices',
+        // {
+        //   type: 'html',
+        //   value: '<hr />'
+        // },
+        // {
+        //   type: 'category',
+        //   label: '릴리스 노트',
+        //   link: {
+        //     type: 'doc',
+        //     id: 'device/xpass_q2/release-notes/index',
+        //   },
+        //   items: [
+        //     'device/xpass_q2/release-notes/100',
+        //   ]
+        // },
+      ],
+    },
+  ],
   xpassd2: [
     {
       type: 'category',
