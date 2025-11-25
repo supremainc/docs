685baa48becde9d30891b6facf8b15b99f8dd58d
minacong <mashin@suprema.co.kr>
2025-11-25 11:28:00 +0900

    릴리스 노트 관련 문서 및 사이드바 항목 삭제

--- sidebars.js
+++ sidebars.js
@@ -1551,21 +1551,6 @@ const sidebars = {
         'device/xpass_q2/product-specifications',
         'device/xpass_q2/regulatory-information',
         'device/xpass_q2/appendices',
-        // {
-        //   type: 'html',
-        //   value: '<hr />'
-        // },
-        // {
-        //   type: 'category',
-        //   label: '릴리스 노트',
-        //   link: {
-        //     type: 'doc',
-        //     id: 'device/xpass_q2/release-notes/index',
-        //   },
-        //   items: [
-        //     'device/xpass_q2/release-notes/100',
-        //   ]
-        // },
       ],
     },
   ],
