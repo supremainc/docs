597da5959eee89857d876421df615f1b53b92712
kj-im <kjim@suprema.co.kr>
2025-11-27 10:09:17 +0900

    사이트 설정에서 페이지 포함 여부를 false로 변경

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -174,7 +174,7 @@ const config = {
           siteDescription: "Check out all of Suprema's products and BioStar related information here.",
           enableLlmsFullTxt: true,
           includeBlog: false,
-          includePages: true,
+          includePages: false,
           includeDocs: true,
           includeVersionedDocs: false, // llms.txt에서는 기본값이 false
           excludeRoutes: [
