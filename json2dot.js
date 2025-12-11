02ed814b107a6d0be2bee24a751e91851b429488
kj-im <kjim@suprema.co.kr>
2025-12-11 12:33:23 +0900

    - 일본어 로케일 파일 추가 - crowdin.yml 파일 설정 수정 - 초기 화면의 develops에 대한 언어 코드 추가 - 초기 화면에서 브라우저 언어에 따른 페이지 언어 설정 변경 로직 추가

--- json2dot.js
+++ json2dot.js
@@ -3,8 +3,8 @@ const path = require('path');
 const {convert} = require('convert-json-to-dot/convert');
 
 // const object = path.join(__dirname, 'src/components/Cmd/x/x.json');
-const object = path.join('C:\\Program Files\\BioStar X\\nginx\\html\\locales\\en\\', 'messages.json');
-const output = path.join(__dirname, 'src/components/Cmd/x/en.json');
+const object = path.join('C:\\Program Files\\BioStar X\\nginx\\html\\locales\\ja\\', 'messages.json');
+const output = path.join(__dirname, 'src/components/Cmd/x/ja.json');
 
 fs.readFile(object, 'utf-8', (err, data) => {
     if (!err) {
