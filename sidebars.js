a43106d3a9245819721efe9e0dcfb7bb4d92d1ed
kj-im <kjim@suprema.co.kr>
2025-12-18 07:40:00 +0900

    AB#84: 사용자 리모트 액세스 제외

--- sidebars.js
+++ sidebars.js
@@ -583,7 +583,6 @@ const sidebars = {
             'platform/biostar_x/settings-visitor',
             'platform/biostar_x/settings-diretory-integration',
             'platform/biostar_x/settings-remote-access',
-            'platform/biostar_x/settings-external-remote-access',
             'platform/biostar_x/settings-virtual-device-event-log'
           ]
         },
