d6580c7e14fa42f39890460d958e21406e4c89b0
kj-im <kjim@suprema.co.kr>
2025-12-24 09:51:38 +0900

    Magnify 컴포넌트에서 지역화된 이미지 경로 처리 개선

--- src/components/Magnify/index.js
+++ src/components/Magnify/index.js
@@ -1,14 +1,20 @@
 import Magnifier from "react-magnifier";
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 
 export default function Magnify(props) {
+    const { i18n: { currentLocale } } = useDocusaurusContext();
     const isDev = process.env.NODE_ENV === 'development';
-    const baseUrl = 'https://supremainc.github.io/docs';
+    const baseUrl = 'https://supremadocs.blob.core.windows.net';
     const img = (() => {
+        const localizedSrc = currentLocale === 'ko' ? 
+            props.src : 
+            props.src.replace('/img/', `/img/en/`);
+        
         if (isDev) {
-            return useBaseUrl(props.src);
+            return useBaseUrl(localizedSrc);
         } else {
-            return `${baseUrl}${props.src}`;
+            return `${baseUrl}${localizedSrc}`;
         }
     })();
     const updatedProps = {
