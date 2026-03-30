d06620c88268dd860164278f77bb452ea4dac57d
kj-im <kjim@suprema.co.kr>
2026-03-30 15:00:52 +0900

    검색 기능 향상을 위한 최종 커밋

--- src/theme/NavbarItem/SearchNavbarItem.js
+++ src/theme/NavbarItem/SearchNavbarItem.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import SearchBar from '@theme/SearchBar';
 import NavbarSearch from '@theme/Navbar/Search';
-import SearchPop from '@theme/SearchPop';
 export default function SearchNavbarItem({mobile, className}) {
   if (mobile) {
     return null;
   }
   return (
     <NavbarSearch className={className}>
-      <SearchPop />
+      <SearchBar />
     </NavbarSearch>
   );
 }
