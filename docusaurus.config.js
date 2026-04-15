2e681c53bbb1218239267ecc239ba86936df55e6
kj-im <kjim@suprema.co.kr>
2026-04-16 08:32:23 +0900

    noIndex 설정 원복

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -41,7 +41,7 @@ const config = {
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: !isPreview ? '/' : '/docs/',
-  noIndex: false,
+  noIndex: !isPreview ? false : true,
   future: {
     v4: {
       removeLegacyPostBuildHeadAttribute: true,
