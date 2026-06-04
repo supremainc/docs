976e8bd88d516a7108e7e29c50349d5baf638781
kj-im <kjim@suprema.co.kr>
2026-06-04 10:27:02 +0900

    초기 화면에 AI Camera 노출하도록 수정

--- src/pages/index.js
+++ src/pages/index.js
@@ -164,12 +164,10 @@ export default function Home() {
         <div className={styles.backBlurPink}></div> */}
         <HomepageHeader />
 
-        {isPreview && (
-          <div className={clsx('container', styles.headsec)}>
-            <Heading as='h2'>AI Camera</Heading>
-          </div>
-        )}
-        {isPreview && <AiCamera />}
+        <div className={clsx('container', styles.headsec)}>
+          <Heading as='h2'>AI Camera</Heading>
+        </div>
+        <AiCamera />
 
         <div className={clsx('container', styles.headsec)}>
           <Heading as='h2'>{translate({message: 'index.productTitle.biometricsDevice'})}</Heading>
