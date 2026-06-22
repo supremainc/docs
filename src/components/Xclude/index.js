455c32c9c37aa6c6f870e88e06734452203b0070
kj-im <kjim@suprema.co.kr>
2026-06-23 08:50:58 +0900

    Include, Xclude 속성을 병렬로 작동하도록 수정

--- src/components/Xclude/index.js
+++ src/components/Xclude/index.js
@@ -12,23 +12,24 @@ export default function XcludeDoc ({children, product, pages, type, ref, lang}) 
     if (product) {
         const prods = Array.isArray(product) ? product : product.split(',');
         const cProd = currentLocale === "ko" ? location.pathname.split("/")[prodSplit] : location.pathname.split("/")[prodSplit + 1];
-        const isProduct = prods.includes(cProd.replace('_ul', ''));
-        
-        return isProduct ? null : <MDXContent>{children}</MDXContent>;
-    } else if (pages) {
+        if (prods.includes(cProd.replace('_ul', ''))) return null;
+    }
+
+    if (pages) {
         const Pages = Array.isArray(pages) ? pages : pages.split(',');
         const cPage = location.pathname.split("/");
         const lastPath = cPage[cPage.length - 1] !== "" ? cPage.length - 1 : cPage.length - 2;
-        const isPage = Pages.includes(cPage[lastPath]);
-        
-        return isPage ? null : <MDXContent>{children}</MDXContent>;
-    } else if (lang) {
-        const cLang = Array.isArray(lang) ? lang : lang.split(',');
-        const isLang = cLang.includes(currentLocale);
-
-        return isLang ? null : <MDXContent>{children}</MDXContent>;
-    } else if (type && ref) {
-        const isMatch = ref.split(',').map(t => t.trim()).includes(type);
-        return isMatch ? null : <MDXContent>{children}</MDXContent>;
+        if (Pages.includes(cPage[lastPath])) return null;
     }
+
+    if (lang) {
+        const cLang = Array.isArray(lang) ? lang : lang.split(',');
+        if (cLang.includes(currentLocale)) return null;
+    }
+
+    if (type && ref) {
+        if (ref.split(',').map(t => t.trim()).includes(type)) return null;
+    }
+
+    return <MDXContent>{children}</MDXContent>;
 }
