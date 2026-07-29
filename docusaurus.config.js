18483d57d42966cb677067fc9061dcfede28bf89
kj-im <kjim@suprema.co.kr>
2026-07-29 13:54:39 +0900

    changelog 알림 배너 추가

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -276,7 +276,14 @@ const config = {
           backgroundColor: '#FFB27D',
           textColor: '#091E42',
           isCloseable: false,
-      }} : null),
+      }} : {
+        announcementBar: {
+          id: 'annoucementbar',
+          content: getLocalizedConfigValue('noticeChanglog'),
+          backgroundColor: '#091E42',
+          textColor: '#fff',
+          isCloseable: false,
+      }}),
       docs: {
         sidebar: {
           hideable: true,
