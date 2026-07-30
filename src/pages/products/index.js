07fed234b2ab6415c78b06ff6e793f48373b1e4f
minacong <mashin@suprema.co.kr>
2026-07-31 08:35:25 +0900

    제품 리디렉션 로직 개선: bs3 삭제 vionyx 추가

--- src/pages/products/index.js
+++ src/pages/products/index.js
@@ -4,27 +4,27 @@ import useBaseUrl from '@docusaurus/useBaseUrl';
 import BrowserOnly from '@docusaurus/BrowserOnly';
 
 export default function Redirect2Product() {
-    const location = useLocation();
-    const search = location.search;
-    // example: ?name=xpassq2
-    const product = search ? `${new URLSearchParams(search).get('name')}` : '';
-    
-    // 외부 URL로 새창에서 열리게 하는 함수
-    const redirectToExternal = (url) => {
-        useEffect(() => {
-            window.open(url, '_blank', 'noopener,noreferrer');
-        }, []);
-        return <div>Opening in new window...</div>;
-    };
-    
-    switch (product) {
-        case 'bs3':
-            return <Redirect to={useBaseUrl(`/device/biostation_3`)} />;
-        case 'bs3m':
-            return <Redirect to={useBaseUrl(`/device/biostation_3_max`)} />;
-        case 'xpq2':
-            return <Redirect to={useBaseUrl(`/device/xpass_q2`)} />;
-        default:
-            return <Redirect to={useBaseUrl(`/`)} />;
-    }
+   const location = useLocation();
+   const search = location.search;
+   // example: ?name=xpassq2
+   const product = search ? `${new URLSearchParams(search).get('name')}` : '';
+   
+   // 외부 URL로 새창에서 열리게 하는 함수
+   const redirectToExternal = (url) => {
+      useEffect(() => {
+         window.open(url, '_blank', 'noopener,noreferrer');
+      }, []);
+      return <div>Opening in new window...</div>;
+   };
+   
+   switch (product) {
+      case 'xpq2':
+         return <Redirect to={useBaseUrl(`/device/xpass_q2`)} />;
+      case 'bs3m':
+         return <Redirect to={useBaseUrl(`/device/biostation_3_max`)} />;
+      case 'vionyx':
+         return <Redirect to={useBaseUrl(`/device/vionyx`)} />;
+      default:
+         return <Redirect to={useBaseUrl(`/`)} />;
+   }
 }
