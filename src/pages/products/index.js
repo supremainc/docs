a1854ea10218e53587b18293130ebe17a5341649
minacong <mashin@suprema.co.kr>
2026-07-31 09:29:07 +0900

    제품 리디렉션 로직 업데이트: 새로운 제품 추가 및 기존 제품 리디렉션 수정

--- src/pages/products/index.js
+++ src/pages/products/index.js
@@ -24,6 +24,42 @@ export default function Redirect2Product() {
          return <Redirect to={useBaseUrl(`/device/biostation_3_max`)} />;
       case 'vionyx':
          return <Redirect to={useBaseUrl(`/device/vionyx`)} />;
+      case 'bew3':
+         return <Redirect to={useBaseUrl(`/device/bioentry_w3`)} />;
+      case 'cs20':
+         return <Redirect to={useBaseUrl(`/device/corestation_20`)} />;
+      case 'di24':
+         return <Redirect to={useBaseUrl(`/device/doorinterface`)} />;
+      case 'xp2':
+         return <Redirect to={useBaseUrl(`/device/xpass_2`)} />;
+      case 'xpd2':
+         return <Redirect to={useBaseUrl(`/device/xpass_d2`)} />;
+      case 'bs2a':
+         return <Redirect to={useBaseUrl(`/device/biostation_2a`)} />;
+      case 'bs3':
+         return <Redirect to={useBaseUrl(`/device/biostation_3`)} />;
+      case 'xs2':
+         return <Redirect to={useBaseUrl(`/device/xstation_2`)} />;
+      case 'bep2':
+         return <Redirect to={useBaseUrl(`/device/bioentry_p2`)} />;
+      case 'ber2':
+         return <Redirect to={useBaseUrl(`/device/bioentry_r2`)} />;
+      case 'bew2':
+         return <Redirect to={useBaseUrl(`/device/bioentry_w2`)} />;
+      case 'cs40':
+         return <Redirect to={useBaseUrl(`/device/corestation_40`)} />;
+      case 'im120':
+         return <Redirect to={useBaseUrl(`/device/inputmodule`)} />;
+      case 'om120':
+         return <Redirect to={useBaseUrl(`/device/outputmodule`)} />;
+      case 'bln2':
+         return <Redirect to={useBaseUrl(`/device/biolite_n2`)} />;
+      case 'fs2':
+         return <Redirect to={useBaseUrl(`/device/facestation_2`)} />;
+      case 'fsf2':
+         return <Redirect to={useBaseUrl(`/device/facestation_f2`)} />;
+      case 'xps2':
+         return <Redirect to={useBaseUrl(`/device/xpass_s2`)} />;
       default:
          return <Redirect to={useBaseUrl(`/`)} />;
    }
