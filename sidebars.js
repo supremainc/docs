929febbcfc34e18d26ae0c52de0b98e1d17f29e8
kj-im <kjim@suprema.co.kr>
2026-08-03 07:37:43 +0900

    주석 처리: Biostation 3 릴리스 노트 144 항목 제거

--- sidebars.js
+++ sidebars.js
@@ -2078,7 +2078,7 @@ function createSidebars(preview) {
             id: 'device/biostation_3/release-notes/index',
           },
           items: [
-            'device/biostation_3/release-notes/144',
+            // 'device/biostation_3/release-notes/144',
             'device/biostation_3/release-notes/143',
             'device/biostation_3/release-notes/142',
             'device/biostation_3/release-notes/141',
