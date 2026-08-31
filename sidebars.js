fb868d9c1e81a372c95b8bb7767947f17d427aa8
suprema-mwkim <mwkim@suprema.co.kr>
2026-08-31 14:05:57 +0900

    103 RN 초안 2차 완료 한영 분리

--- sidebars.js
+++ sidebars.js
@@ -1066,7 +1066,7 @@ function createSidebars(preview) {
             id: 'platform/biostar_x/release-notes'
           },
           items: [
-            'platform/biostar_x/release-notes/103-draft',
+            'platform/biostar_x/release-notes/103',
             'platform/biostar_x/release-notes/102',
             'platform/biostar_x/release-notes/101',
             'platform/biostar_x/release-notes/100',
