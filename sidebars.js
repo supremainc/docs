2af8d3b82dfa9dfddc39f350e2efc3b7a40fb8ee
suprema-mwkim <mwkim@suprema.co.kr>
2026-09-21 15:56:54 +0900

    151 RN 초안 완료

--- sidebars.js
+++ sidebars.js
@@ -2093,6 +2093,7 @@ function createSidebars(preview) {
             id: 'device/biostation_3/release-notes/index',
           },
           items: [
+            'device/biostation_3/release-notes/151',
             'device/biostation_3/release-notes/150',
             'device/biostation_3/release-notes/143',
             'device/biostation_3/release-notes/142',
